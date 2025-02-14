0e3ba76b37f887a67c1764b8f2bbdb0404c683ba
nicollechoy <nicollefrance29@gmail.com>
2025-02-14 12:59:11 +0800

    fix (landing page): fixed linking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Add event listeners to images if needed
+// Add event listeners to images to navigate to question page
 const polaroids = document.querySelectorAll('.polaroid');
 
 polaroids.forEach(polaroid => {
     polaroid.addEventListener('click', () => {
-        window.location.href = 'question.html'; // Go to the question page
+        window.location.href = '/question/index.html'; // Fixed the path
     });
 });
